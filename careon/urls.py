2f9275486e1687397fe59066751114a30d97d897
soojung <crystal007002@naver.com>
2025-11-26 11:27:57 +0900

    feat: 파일 업로드 endpoint for calendar attachments 개발

--- careon/urls.py
+++ careon/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -29,3 +32,5 @@ urlpatterns = [
     path("", include("calender.urls")),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
